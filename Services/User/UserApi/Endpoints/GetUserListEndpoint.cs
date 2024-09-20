5ca6e90e91edf5795d8fb8619a1aeca65e2c9630
Brocker591 <60329786+Brocker591@users.noreply.github.com>
2024-09-21 01:21:23 +0200

    UserApi angelegt

---
+++ Services/User/UserApi/Endpoints/GetUserListEndpoint.cs
@@ -0,0 +1,65 @@
+﻿using Common.Keycloak;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace UserApi.Endpoints;
+
+public record AdminUserDto(string username, string password);
+public record UserModel(Guid id, string username, string firstname, string lastname, string email);
+
+public static class GetUserListEndpoint
+{
+    public static IEndpointRouteBuilder MapGetUserListEndpoint(this IEndpointRouteBuilder routes)
+    {
+        routes.MapPost("/GetUsers", async (AdminUserDto dto, IConfiguration config) =>
+        {
+            try
+            {
+                string userUrl = config.GetSection("KeycloakUserSetting:UserUrl").Value;
+                string urlToken = config.GetSection("KeycloakUserSetting:TokenUrl").Value;
+                string clientId = config.GetSection("KeycloakSetting:Audience").Value;
+                string clientSecret = config.GetSection("KeycloakUserSetting:ClientSecret").Value;
+
+
+                var httpClient = new HttpClient();
+
+                var data = new Dictionary<string, string>
+                {
+                    {"username", dto.username},
+                    {"password", dto.password},
+                    {"client_id", clientId},
+                    {"client_secret" ,clientSecret},
+                    {"grant_type", "password"},
+                };
+
+                var content = new FormUrlEncodedContent(data);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+                var response = await httpClient.PostAsync(urlToken, content);
+                string responseContent = await response.Content.ReadAsStringAsync();
+                JObject jobject = JObject.Parse(responseContent);
+
+                var request = new HttpRequestMessage(HttpMethod.Get, userUrl);
+                request.Headers.Add("Authorization", "Bearer " + jobject["access_token"].ToString());
+                var responseUsers = await httpClient.SendAsync(request);
+                string contentUsers = await responseUsers.Content.ReadAsStringAsync();
+                List<UserModel> users = JsonConvert.DeserializeObject<List<UserModel>>(contentUsers);
+
+                return Results.Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(detail: "Fehler bei GetUsers", statusCode: StatusCodes.Status500InternalServerError);
+            }
+        })
+        .WithName("GetUsers")
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithSummary("Get Users")
+        .WithDescription("Get Users")
+        .RequireAuthorization();
+
+        return routes;
+    }
+}
---
+++ Services/User/UserApi/Program.cs
@@ -0,0 +1,46 @@
+using Common.Keycloak;
+using UserApi.Endpoints;
+
+const string corsSettings = "AllowedOrigin";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+builder.Services.AddAuthorization();
+builder.Services.AddKeycloak(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsBuilder =>
+    {
+        var allowedOrigin = builder.Configuration[corsSettings] ?? throw new InvalidOperationException("AllowedOrigin is not set");
+
+        corsBuilder.WithOrigins(allowedOrigin).AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
+
+var app = builder.Build();
+
+
+app.MapGetUserListEndpoint();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.UseAuthorization();
+
+app.UseCors();
+app.Run();
+
+
