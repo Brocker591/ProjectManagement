69c67dae8fa3e58b5b0b58fe8f0137de345cfd2f
Brocker591 <60329786+Brocker591@users.noreply.github.com>
2025-03-08 21:46:07 +0100

    User werden anhand der Group zurückgegeben

--- Services/User/UserApi/Endpoints/GetUserListEndpoint.cs
+++ Services/User/UserApi/Endpoints/GetUserListEndpoint.cs
@@ -1,22 +1,27 @@
-﻿using Common.Keycloak;
+﻿using Common.Authorization;
+using Common.Authorization.Models;
+using Common.Keycloak;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace UserApi.Endpoints;
 
 public record UserModel(Guid id, string username, string firstname, string lastname, string email);
+public record GroupModel(Guid id, string name, string path);
 
 public static class GetUserListEndpoint
 {
     public static IEndpointRouteBuilder MapGetUserListEndpoint(this IEndpointRouteBuilder routes)
     {
-        routes.MapGet("/GetUsers", async (IConfiguration config) =>
+        routes.MapGet("/GetUsers", async (IConfiguration config, ClaimsPrincipal user) =>
         {
             try
             {
                 string userUrl = config.GetSection("KeycloakUserSetting:UserUrl").Value;
+                string groupUrl = config.GetSection("KeycloakUserSetting:GroupUrl").Value;
                 string urlToken = config.GetSection("KeycloakUserSetting:TokenUrl").Value;
                 string clientId = config.GetSection("KeycloakUserSetting:ClientId").Value;
                 string clientSecret = config.GetSection("KeycloakUserSetting:ClientSecret").Value;
@@ -34,18 +39,35 @@ public static class GetUserListEndpoint
                     {"grant_type", "password"},
                 };
 
+                IdentifiedUser? identifiedUser = user.IdentifyUser();
+
+                if (identifiedUser is null)
+                {
+                    return Results.Unauthorized();
+                }
+
                 var content = new FormUrlEncodedContent(data);
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
 
                 var response = await httpClient.PostAsync(urlToken, content);
                 string responseContent = await response.Content.ReadAsStringAsync();
                 JObject jobject = JObject.Parse(responseContent);
+                string bearerToken = jobject["access_token"].ToString();
 
-                var request = new HttpRequestMessage(HttpMethod.Get, userUrl);
-                request.Headers.Add("Authorization", "Bearer " + jobject["access_token"].ToString());
-                var responseUsers = await httpClient.SendAsync(request);
-                string contentUsers = await responseUsers.Content.ReadAsStringAsync();
-                List<UserModel> users = JsonConvert.DeserializeObject<List<UserModel>>(contentUsers);
+                List<GroupModel>? groups = await GetGroupFromUserId(userUrl, httpClient, identifiedUser, bearerToken);
+
+                if(groups is null)
+                {
+                    return Results.Problem(detail: "Fehler keine Group gefunden", statusCode: StatusCodes.Status500InternalServerError);
+                }
+
+                List<UserModel>? users = await GetUserListFromGroup(groupUrl, httpClient, groups[0], bearerToken);
+
+                if(users is null)
+                {
+                    return Results.Ok(new List<UserModel>());
+                }
+
 
                 return Results.Ok(users);
             }
@@ -61,5 +83,27 @@ public static class GetUserListEndpoint
         .RequireAuthorization();
 
         return routes;
+    }
+
+    private static async Task<List<GroupModel>?> GetGroupFromUserId(string userUrl, HttpClient httpClient, IdentifiedUser identifiedUser, string bearerToken)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{userUrl}/{identifiedUser.UserId.ToString()}/groups");
+        request.Headers.Add("Authorization", "Bearer " + bearerToken);
+        var responseGroup = await httpClient.SendAsync(request);
+        string contentGroup = await responseGroup.Content.ReadAsStringAsync();
+        List<GroupModel>? groups = JsonConvert.DeserializeObject<List<GroupModel>>(contentGroup);
+
+        return groups;
+    }
+
+    private static async Task<List<UserModel>?> GetUserListFromGroup(string groupUrl, HttpClient httpClient, GroupModel groupModel, string bearerToken)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{groupUrl}/{groupModel.id.ToString()}/members");
+        request.Headers.Add("Authorization", "Bearer " + bearerToken);
+        var responseUser = await httpClient.SendAsync(request);
+        string contentUser = await responseUser.Content.ReadAsStringAsync();
+        List<UserModel>? users = JsonConvert.DeserializeObject<List<UserModel>>(contentUser);
+
+        return users;
     }
 }
